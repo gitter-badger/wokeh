c4d19929f00f6861bf0c61aedbb8d21c511a3ff4
Rahmat Subekti <rahmatsubekti@live.com>
2016-03-19 20:20:16 +0700

    Initial Commit

---
+++ Wokeh/Functions.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Windows;
+
+namespace Wokeh
+{
+    class Functions
+    {
+        public string encodeHTML()
+        {
+            string text = Clipboard.GetText();
+            string enc = WebUtility.HtmlEncode(text);
+            Clipboard.SetText(enc);
+            return enc;
+        }
+        public string decodeHTML()
+        {
+            string text = Clipboard.GetText();
+            string dec = WebUtility.HtmlDecode(text);
+            Clipboard.SetText(dec);
+            return dec;
+        }
+        public string toBase64()
+        {
+            OpenFileDialog dOpen = new OpenFileDialog();
+            dOpen.Filter = "Image Files|*.bmp;*.png;*.jpg;*.jpeg;*.gif;";
+            Nullable<bool> y = dOpen.ShowDialog();
+            if (y == true)
+            {
+                string loc = dOpen.FileName;
+                string ext = Path.GetExtension(loc).Replace(".", "");
+
+                using (Image img = Image.FromFile(loc))
+                {
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        img.Save(ms, img.RawFormat);
+                        byte[] imgBytes = ms.ToArray();
+                        string imgString = "data:image/"+ext+";base64," + Convert.ToBase64String(imgBytes);
+                        Clipboard.SetText(imgString);
+                        return imgString;
+                    }
+                }
+            }
+            else
+            {
+                return "No image Selected";
+            }
+        }
+    }
+}
---
+++ Wokeh/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Wokeh
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool pinS;
+        int switchF = 0;
+        Functions fun = new Functions();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TitleWindow_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void menu_Click(object sender, RoutedEventArgs e)
+        {
+            (sender as Button).ContextMenu.IsEnabled = true;
+            (sender as Button).ContextMenu.PlacementTarget = (sender as Button);
+            (sender as Button).ContextMenu.Placement = System.Windows.Controls.Primitives.PlacementMode.Bottom;
+            (sender as Button).ContextMenu.IsOpen = true;
+        }
+
+        private void btnClose_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnPin_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (pinS == true)
+            {
+                this.Topmost = false;
+                pinS = false;
+                btnPin.Foreground = Brushes.Black;
+            }
+            else
+            {
+                this.Topmost = true;
+                pinS = true;
+                btnPin.Foreground = Brushes.Red;
+            }
+            
+        }
+
+        private void htmlEncode_Click(object sender, RoutedEventArgs e)
+        {
+            switchF = 0;
+            TitleWindow.Content = "Wokeh HTML Encoder";
+            action.Content = "Encode";
+        }
+
+        private void htmlDecode_Click(object sender, RoutedEventArgs e)
+        {
+            switchF = 1;
+            TitleWindow.Content = "Wokeh HTML Decoder";
+            action.Content = "Decode";
+        }
+
+        private void imgEncode_Click(object sender, RoutedEventArgs e)
+        {
+            switchF = 2;
+            TitleWindow.Content = "Wokeh Image Encoder";
+            action.Content = "Open File";
+        }
+        
+        private void action_Click(object sender, RoutedEventArgs e)
+        {
+            if (switchF == 0)
+            {
+                preview.Text = fun.encodeHTML();
+            }
+            else if (switchF == 1)
+            {
+                preview.Text = fun.decodeHTML();
+            }
+            else if (switchF == 2)
+            {
+                preview.Text = fun.toBase64();
+            }
+            else
+            {
+                preview.Text = "Opps!";
+            }
+        }
+
+        private void preview_PreviewMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            preview.Text = "Wokeh\n"+
+                "A Simple Offline Blogger Tool\n\n"+
+                "Tunggu dulu, kamu bener - bener pake alat ini?\n"+
+                "Ahh, Aku cuma ga nyangka aplikasi ini berguna.\n\n"+
+                "Jika kamu anggap aplikasi ini benar - benar berguna, Beri bintang atau ikut berkontribusi disini:\n"+
+                "https://github.com/rmsubekti/wokeh\n\n" +
+                "Atau bantu Laporkan Error / kesalahan :\n"+
+                "https://github.com/rmsubekti/wokeh/issues";
+        }
+    }
+}
